afd870e8188a2825fa629602c52a6cc8369a4e85
sarahmhd <sarahmhd213199@gmail.com>
2023-12-23 15:32:11 +0200

    skills section

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -21,15 +21,15 @@ const Resume = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-briefcase"
+                  className="icon icon-tabler icon-tabler-briefcase"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
@@ -53,15 +53,15 @@ const Resume = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-briefcase"
+                  className="icon icon-tabler icon-tabler-briefcase"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
@@ -89,15 +89,15 @@ const Resume = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-school"
+                  className="icon icon-tabler icon-tabler-school"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
@@ -119,15 +119,15 @@ const Resume = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-school"
+                  className="icon icon-tabler icon-tabler-school"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,7 +1,85 @@
 import React from "react";
+import styles from "./skills.module.css";
+
+const skillsData = [
+  {
+    id: 1,
+    title: "html",
+    progress: "85",
+  },
+  {
+    id: 2,
+    title: "css",
+    progress: "85",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    progress: "75",
+  },
+  {
+    id: 4,
+    title: "Bootstrap",
+    progress: "80",
+  },
+  {
+    id: 5,
+    title: "Tailwind",
+    progress: "70",
+  },
+  {
+    id: 6,
+    title: "SASS",
+    progress: "70",
+  },
+  {
+    id: 7,
+    title: "React js",
+    progress: "80",
+  },
+  {
+    id: 8,
+    title: "mui",
+    progress: "65",
+  },
+];
 
 const Skills = () => {
-  return <div>Skills Skills</div>;
+  return (
+    <div className={`${styles.skills} section-p`}>
+      <div className="container mx-auto">
+        <div
+          className={`${styles.skills_content} flex flex-col justify-center items-center`}
+        >
+          <h2 className="section_title">Skills</h2>
+          <div
+            className={`${styles.content} ${styles.progress_circle} grid grid-cols-12 justify-center gap-10`}
+          >
+            {skillsData.map((skill) => (
+              <div
+                className={`${styles.progress} col-span-12 sm:col-span-6  md:col-span-4 lg:col-span-3 mx-auto`}
+                key={skill.id}
+              >
+                <span
+                  className={`${styles.title} ${styles.timer} flex flex-col justify-center items-center`}
+                  data-from="0"
+                  data-to="85"
+                  data-speed="1800"
+                >
+                  <span className={`${styles.skill_val}`}>
+                    {skill.title} <br /> <span>{skill.progress}%</span>
+                  </span>
+                </span>
+                <div className={styles.overlay}></div>
+                <div className={styles.left}></div>
+                <div className={styles.right}></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
